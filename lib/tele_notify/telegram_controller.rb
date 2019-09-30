f0d09b64e9a43ea1b2e083aaed29e8488b14929d
rvld <vs@account>
2019-09-30 22:16:30 +0800

    push

--- lib/tele_notify/telegram_controller.rb
+++ lib/tele_notify/telegram_controller.rb
@@ -2,13 +2,11 @@ module TeleNotify
   module Controller
     def webhook
       if params[:message]
-        user = TelegramUser.create( { telegram_id: params[:message][:from][:id],
+        TelegramUser.create( { telegram_id: params[:message][:from][:id],
                                       first_name: params[:message][:from][:first_name],
                                       username: params[:message][:from][:username],
                                       last_name: params[:message][:from][:last_name]})
-        if user
-          tes
-        end
+
         render :nothing => true, :status => :ok
       end
     end
